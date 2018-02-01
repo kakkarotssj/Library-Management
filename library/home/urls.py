c0ffc10f45b02ad7cf552340139d4fc987089a04
kakkarotssj <kanjeckcooldeva.005@gmail.com>
2018-02-02 02:12:03 +0530

    login functionality added but through bad practice

--- library/home/urls.py
+++ library/home/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^signup$', views.signup, name='signup'),
+    url(r'^login$', views.login, name='login'),
+    url(r'^student_profile$', views.student_profile, name='student_profile'),
+    url(r'^logout$', views.logout, name='logout'),
 ]
--- library/home/views.py
+++ library/home/views.py
@@ -3,8 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from search.models import Student
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
+
+login_status = False
 
 
 def home(request):
@@ -28,11 +31,16 @@ def signup(request):
 			
 			signup_status = register_student(student_signup)
 			context['signup_status'] = signup_status
+			if signup_status == "Registration Failed for some reason. Try again":
+				return render(request, 'home/signup.html', context)
+			
+			return HttpResponseRedirect('/login')
 
 		except UnboundLocalError:
 			pass
 
 	return render(request, 'home/signup.html', context)
+
 
 
 def register_student(student_signup):
@@ -51,3 +59,39 @@ def register_student(student_signup):
 		return "Registration Failed for some reason. Try again"
 
 	return "Registration done successfully"
+
+
+def login(request):
+	context = {}
+
+	if request.method == "POST":
+		students = Student.objects.all()
+		for student in students:
+			if student.user_id == request.POST['user_id']:
+				if student.password == request.POST['password']:
+					global login_status
+					login_status = True
+					return HttpResponseRedirect('/student_profile')
+				else:
+					context['error'] = "Incorrect password. Try again."
+		context['error'] = "You are not registered yet."
+
+	return render(request, 'home/login.html', context)
+
+
+def student_profile(request):
+	context = {}
+
+	print login_status
+	context['login_status'] = login_status
+
+	return render(request, 'home/student_profile.html', context)
+
+
+def logout(request):
+	context = {}
+
+	global login_status
+	login_status = False
+
+	return HttpResponseRedirect('/')
